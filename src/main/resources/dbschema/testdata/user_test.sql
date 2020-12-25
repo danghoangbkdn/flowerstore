5bdf6dd61e639bd46bf0ee1834a911c205d55d44
danghoangbkdn <danghoanght98@gmail.com>
2020-12-25 07:35:45 +0700

    fixbug

--- src/main/resources/dbschema/testdata/user_test.sql
+++ src/main/resources/dbschema/testdata/user_test.sql
@@ -1,6 +1,6 @@
 INSERT INTO public.user (email, password, firstname, lastname)
-VALUES ('admin@gmail.com','$2a$10$8Cof0cOQrqgVy.Pa/dDRW.8ZTm9kltOS4kodL0qW9TFewHwe/AUcW','Gin','Do');
+VALUES ('admin@gmail.com','$2a$10$8Cof0cOQrqgVy.Pa/dDRW.8ZTm9kltOS4kodL0qW9TFewHwe/AUcW','Ginn','Do');
 INSERT INTO public.user (email, password, firstname, lastname)
-VALUES ('dddnhi@gmail.com','$2a$10$8Cof0cOQrqgVy.Pa/dDRW.8ZTm9kltOS4kodL0qW9TFewHwe/AUcW','nhi','nguyen');
+VALUES ('dddnhi@gmail.com','$2a$10$8Cof0cOQrqgVy.Pa/dDRW.8ZTm9kltOS4kodL0qW9TFewHwe/AUcW','nhin','nguyen');
 INSERT INTO public.user (email, password, firstname, lastname)
-VALUES ('kai@gmail.com','$2a$10$8Cof0cOQrqgVy.Pa/dDRW.8ZTm9kltOS4kodL0qW9TFewHwe/AUcW','Kai','Do');
+VALUES ('kai@gmail.com','$2a$10$8Cof0cOQrqgVy.Pa/dDRW.8ZTm9kltOS4kodL0qW9TFewHwe/AUcW','Kain','Do');
